edd97a49b21963dbf6de9ca1a8603008ff5550a3
olgklsn <lumediluna007@gmail.com>
2026-01-11 23:24:02 +0300

    add hover button and link

--- screen/tests/managerAuth.test.js
+++ screen/tests/managerAuth.test.js
@@ -1,17 +1,60 @@
-
 import { test, expect } from '@playwright/test';
 
-test('страница manager.itoolabs-stage2 рендерится корректно', async ({ page }) => {
-  // Arrange: открыть нужный урл
+const managerAuth = 'https://manager.itoolabs-stage2.exolve.ru';
+
+
+
+test('Экран авторизации под менеджером: главная + hover кнопки "Войти"', async ({ page }) => {
+  await page.goto((managerAuth), {
+    waitUntil: 'networkidle',
+  });
+
+  const loginButton = page.getByRole('button', { name: 'Войти', exact: true });
+
+  await expect(loginButton).toBeVisible();
+
+  const loginBox = await loginButton.boundingBox();
+  if (loginBox) {
+    await expect(page).toHaveScreenshot('manager-login-static.png', {
+      fullPage: false,
+      clip: loginBox,
+    });
+  }
+
+  await loginButton.hover();
+  const loginHoverBox = await loginButton.boundingBox();
+  if (loginHoverBox) {
+    await expect(page).toHaveScreenshot('manager-login-hover.png', {
+      fullPage: false,
+      clip: loginHoverBox,
+    });
+  }
+});
+
+test('Экран авторизации под менеджером: hover ссылки "Восстановление пароля"', async ({ page }) => {
   await page.goto('https://manager.itoolabs-stage2.exolve.ru/#/', {
-    waitUntil: 'networkidle', // подождать, пока всё догрузится
+    waitUntil: 'networkidle',
   });
 
-  // можно подождать появления какого-то основного элемента, если знаешь селектор
-  // await page.waitForSelector('селектор_основного_блока');
+  // Это span.link-text с текстом "Восстановление пароля"
+  const restoreLinkText = page.getByText('Восстановление пароля', { exact: true });
 
-  // Act + Assert: сравнить со скриншотом-эталоном
-  await expect(page).toHaveScreenshot('manager-main.png', {
-    fullPage: true,
-  });
+  await expect(restoreLinkText).toBeVisible();
+
+  const box = await restoreLinkText.boundingBox();
+  if (box) {
+    // статичное состояние
+    await expect(page).toHaveScreenshot('manager-restore-static.png', {
+      fullPage: false,
+      clip: box,
+    });
+
+    // наведение мыши по координатам этого текста
+    await page.mouse.move(box.x + box.width / 2, box.y + box.height / 2);
+
+    await expect(page).toHaveScreenshot('manager-restore-hover.png', {
+      fullPage: false,
+      clip: box,
+    });
+  }
 });
